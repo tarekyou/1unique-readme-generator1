90ca0f3408d61d81fffe1f2386401e5c2ab7b43a
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-11-08 14:42:28 -0800

    wrote write file function

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require ('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown.js')
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions =  () => {
     return inquirer.prompt([
@@ -134,7 +135,22 @@ const questions =  () => {
 };    
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
+
+const writeFile = fileContent =>{
+    return new Promise((resolve, reject)=>{
+        fs.writeFile('./dist/Readme.md', fileContent, err =>{
+            if(err){
+                reject(err)
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            })
+        })
+    });
+};
 
 // TODO: Create a function to initialize app
 function init() {
